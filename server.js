a8a6c0c6c25f23243d70d65af30916db92c5522d
pablitorg <pablitorg@usal.es>
2023-11-14 12:39:43 +0100

    Modificacion de texto

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (req, res) => res.send('Hola Mundo con Express'))
+app.get('/', (req, res) => res.send('<h1>Express con HTML</h1>'))
 
 app.listen(3000);
 console.log('Servidor en el puerto 3000');
